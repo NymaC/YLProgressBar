079ee43e8cf68ac64dc183ab15fe24ce90ebc3c4
Bastien Levasseur <bastien.levasseur_ext@michelin.com>
2022-02-17 10:45:16 +0100

    Add Package.swift for SPM compatibility

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "YLProgressBar",
+    platforms: [
+        .iOS("8.3"), .tvOS("9.0")
+    ],
+    products: [
+        .library(
+            name: "YLProgressBar",
+            targets: ["YLProgressBar"]),
+    ],
+    targets: [
+        .target(
+            name: "YLProgressBar")
+    ]
+)
