d62742341a8806a8c098b6bc530dd409560c83e9
Bastien Levasseur <bastien.levasseur_ext@michelin.com>
2022-02-17 11:29:25 +0100

    Debug Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "YLProgressBarSample",
+            name: "YLProgressBar",
             targets: ["YLProgressBar"]),
     ],
     targets: [
